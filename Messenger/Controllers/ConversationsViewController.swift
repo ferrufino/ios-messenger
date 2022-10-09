d0b4051222ffb8a7c33b70eeb9593d0e3f5455b0
Ferrufino <ferrufinodlf@gmail.com>
2022-10-08 21:29:47 -0700

    Add login and register with Firebase auth and db

---
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Gustavo Ferrufino on 2022-08-28.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ConversationsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        validateAuth()
+    }
+    
+    private func validateAuth() {
+        
+        if FirebaseAuth.Auth.auth().currentUser == nil {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+    
+}
+
---
+++ Messenger/Resources/Extensions.swift
@@ -0,0 +1,29 @@
+//
+//  Extensions.swift
+//  Messenger
+//
+//  Created by Gustavo Ferrufino on 2022-08-28.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    public var bottom: CGFloat {
+        return self.frame.size.height + top
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+}
